d4b5f402f5d813db98959533410853f270f15410
Kamijouko <MikuVick7139@gmail.com>
2021-11-30 05:10:55 +0900

    添加项目文件。

---
+++ KamijoukosLibrary/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamijoukosLibrary
+{
+    public class Base
+    {
+        public void Tick()
+        {
+
+        }
+    }
+}
